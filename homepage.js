166b94b4efca4dd066083667950d13f6d58357bf
Oluwatomisin <kanmitomi@gmail.com>
2023-11-28 13:16:28 +0100

    first commit

--- homepage.js
+++ homepage.js
@@ -24,3 +24,46 @@ function show() {
 
 document.getElementById("na").innerHTML = (sessionStorage.getItem("nam"));
 
+// Search bar test
+
+var html = ["html", "htm", "ht", "h"]
+
+var css = ["css", "cs"]
+
+var js = ["javascript", "javascrip", "javascri", "javascr", "javascr", "javasc"]
+
+var python = ["python", "pytho", "pyth", "pyt", "py", "p"]
+
+var fol = ["html", "css", "javascript", "python", "c", "c++"]
+
+
+
+document.getElementById("search-btn").addEventListener("click", search);
+
+function search (){
+
+    var ff = document.getElementById("search-bar").value    
+
+    if( ff = " "){
+        document.getElementById("e").innerHTML= "Fill in a course";
+    }
+
+    else if ((html.includes(ff))){
+        alert("seen")
+    }
+
+    else if ((css.includes(ff))){
+        alert("css")
+    }
+
+    else if ((js.includes(ff))){
+        alert("javascript")
+    }
+
+    else if ((python.includes(ff))){
+        alert("python")
+    }
+
+    else if (ff !== "html", ff !== "css", ff !== "javascript")
+        alert("Course not found")
+}
